50dfeef5b8ee2d3e5a3b56db57249cc8b1a92527
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-18 09:11:07 -0700

    add image of specs

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("form#ping-pong-form").submit(function(event){
+    event.preventDefault();
+    var userInput = parseInt($("input#number").val.());
+
+    
+  });
+});

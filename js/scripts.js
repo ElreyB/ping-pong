9dd913b94b54d4f2edb6e9e9c990bf7672e680ec
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-18 09:37:58 -0700

    pass specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
+function appendResult(result){
+  if ((result % 3 === 0) && (result % 5 === 0)){
+    return $(".results").append('<li>pingpong</li>');
+  } else if (result % 5 === 0){
+    return $(".results").append('<li>pong</li>');
+  } else if (result % 3 === 0){
+    return $(".results").append('<li>ping</li>');
+  } else {
+    return $(".results").append('<li>' + String(result) + '</li>');
+  }
+}
+
+
 $(document).ready(function(){
   $("form#ping-pong-form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("input#number").val.());
+    var userInput = parseInt($("input#number").val());
 
-    
+    for (i = 1; i <= userInput; i++){
+      appendResult(i);
+    }
+
   });
 });

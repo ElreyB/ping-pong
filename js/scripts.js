86eee2d54f6b4e09da22893a1c73754f038d0126
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-18 12:32:07 -0700

    add animation to show on li at a time

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 function appendResult(result){
   if ((result % 3 === 0) && (result % 5 === 0)){
-    return $(".results").append('<li>pingpong</li>');
+    return $(".results").append('<li class="animation"><div id="frame">🏓 🏓</div></li>');
   } else if (result % 5 === 0){
-    return $(".results").append('<li>pong</li>');
+    return $(".results").append('<li class="animation"><div id="frame">🏓</div></li>');
   } else if (result % 3 === 0){
-    return $(".results").append('<li>ping</li>');
+    return $(".results").append('<li class="animation"><div id="frame">🏓</div></li>');
   } else {
-    return $(".results").append('<li>' + String(result) + '</li>');
+    return $(".results").append('<li class="animation"><div id="frame">' + String(result) + '</div></li>');
   }
 }
+
 
 
 $(document).ready(function(){
@@ -19,10 +20,19 @@ $(document).ready(function(){
     for (i = 1; i <= userInput; i++){
       appendResult(i);
     }
+
+// animation http://jsfiddle.net/adeneo/ft9tb2cb/
+    (function showpanel(i, elems) {
+        i = i === elems.length-1 ? 0 : i+1;
+        elems.eq(i).show(1).delay(1000).hide(1, function() {
+            showpanel(i, elems);
+        });
+    })(-1, $("ul#list > li"));
+    showpanel(1, "ol");
   });
 
   $("#clear").click(function(event){
     event.preventDefault();
-    $("ul").empty();
+    $("").empty();
   });
 });
